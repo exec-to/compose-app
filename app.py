fdfa7ddc0776e6737ac6701f809d76c1ad1cb36e
Roman Sokolov <header.cpp@gmail.com>
2021-05-29 21:36:57 +0700

    update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Deploy by Jenkins! I have been seen {} times.\n'.format(count)
+    return 'Updated at {}</br>Deploy by Jenkins! I have been seen {} times.\n'.format(time.time, count)
